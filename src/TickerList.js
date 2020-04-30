ae0ee5ef4250cc80f13054b4c8122c8b8959d629
hnariman <hnariman@gmail.com>
2020-04-30 20:50:10 +0400

    added styled-components

--- src/TickerList.js
+++ src/TickerList.js
@@ -1,53 +1,71 @@
 import React from 'react';
-import './TickerList.css';
-
-const data =  [ 
-  { 
-    "id": "1", 
-    "userId": "1", 
-    "code": "AAPL", 
-    "amount": "3", 
-    "purchasePrice": "125.00" }, 
-  { 
-    "id": "1", 
-    "userId": "1", 
-    "code": "AAPL", 
-    "amount": "3", 
-    "purchasePrice": "125.00" }, 
-  { 
-    "id": "2", 
-    "userId": "2", 
-    "code": "NKE", 
-    "amount": "1", 
-    "purchasePrice": "737.00" }
-]
+import styled from 'styled-components'
+{/* import Decimal from './Decimal.js'; */}
 
 class TickerList extends React.Component {
+  static defaultProps = {
+    data :  [ 
+      { 
+        'id': '1', 
+        'userId': '1', 
+        'code': 'AAPL', 
+        'amount': '3', 
+        'purchasePrice': '125.00' }, 
+      { 
+        'id': '2', 
+        'userId': '2', 
+        'code': 'NKE', 
+        'amount': '1', 
+        'purchasePrice': '737.00' }
+    ]
+  }
   state = { pageNum:1, items:4 }
   formatMe = (x) => { return Number(x).toFixed(2);}
   render(){
   const {items, pageNum} = this.state;
     return(
-      <div className="tickerList">
-        <div>{data.slice(items * (pageNum-1), pageNum * items)
+      <section className='tickerList'>
+        <div className='results'>{this.props.data.slice(items * (pageNum-1), pageNum * items)
             .map( each =>
-            <ul className='each-ticker' key={each.code}>
-              <li className="code"   > {each.code} </li>
-              <li className="company"> Company Name Inc. </li>
-              <li className="amount" > {each.amount} pcs </li>
-              <li className="price"  > {this.formatMe(each.purchasePrice)}$ </li>
-              <li className="change" > daily change % </li>
-            </ul> 
+            <Ticker   className='each-ticker' key={each.code}>
+              <Detail className='code'   > {each.code} </Detail>
+              <Detail className='company'> Company Name Inc. </Detail>
+              <Detail className='amount' > {each.amount} pcs </Detail>
+              <Detail className='price'  > {this.formatMe(each.purchasePrice)}$ </Detail>
+              <Detail className='change' > daily change % </Detail>
+
+              {/* <Detail className='code'   ><Decimal value={each.code} /></li> */}
+              {/* <Detail className='company'> Company Name Inc.</Detail > */}
+              {/* <Detail className='amount' ><Decimal value={each.amount} /></Detail > */}
+              {/* <Detail className='price'  ><Decimal value={each.purchasePrice} /></Detail > */}
+              {/* <Detail className='change' ><Decimal value={each.change} /></Detail > */}
+            </Ticker> 
             )}
           </div>
-          <ul className="pages">
-            <li><button onClick={this.switchPage}>{pageNum}</button></li>
-            <li><button onClick={this.switchPage}>{pageNum+1}</button></li>
-            <li><button onClick={this.switchPage}>{pageNum+2}</button></li>
-          </ul>
-      </div>
+          <Pages className='pages'>
+            <Page onClick={this.switchPage}>{pageNum}</Page>
+            <Page onClick={this.switchPage}>{pageNum+1}</Page>
+            <Page onClick={this.switchPage}>{pageNum+2}</Page>
+          </Pages>
+        </section>
     )
   }
 }
+
+const Page = styled.button`
+  font-size:1.2rem;
+  border:none;
+  background: white;
+  cursor:pointer;
+  &:hover{ opacity:0.5; } `;
+const Ticker = styled.ul`
+  margin:2px auto;
+  width: 40vw;
+  height:18vh;
+  line-height:18vh;
+  border:1px solid black;
+  &:hover{ background:#ffcccc; } `;
+const Detail = styled.li` display: inline-block; margin-right: 50px; `;
+const Pages = styled.ul` width:10vw; margin: 0 auto; `;
 
 export default TickerList;
